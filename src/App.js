610168ef985210582b7c8e722bcb6f9c9de605e9
SoMin <thals0107@naver.com>
2022-09-03 10:18:08 +0900

    Blog made with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // let post = "강남 우동 맛집";
+  // state
+  let [title, b] = useState([
+    "여자 코트 추천",
+    "강남 우동 맛집",
+    "파이썬 독학",
+  ]);
+  let [like, setLike] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+        <h4>BLOG</h4>
+      </div>
+      {/* <h4>{post}</h4> */}
+      <button
+        onClick={() => {
+          let copy = [...title];
+          b(copy.sort());
+          // console.log(title);
+          // console.log(copy);
+        }}
+      >
+        글자순 정렬
+      </button>
+      <div className="list">
+        <h4 onClick={() => {}}>
+          {title[0]}
+          <span
+            onClick={() => {
+              setLike(like + 1);
+            }}
+          >
+            👍
+          </span>
+          {like}
+        </h4>
+        <p>9월 3일 발행</p>
+      </div>
+
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>9월 3일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>9월 3일 발행</p>
+      </div>
+
+      <Modal></Modal>
+    </div>
+  );
+}
+
+// 상세페이지
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>Title</h4>
+      <p>Date</p>
+      <p>Details</p>
     </div>
   );
 }
